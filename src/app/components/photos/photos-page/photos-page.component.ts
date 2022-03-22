ed926865c45bf6483214cd326bdaae6e67987666
Sankalp Sinha <sankalpsinha@hey.com>
2022-03-22 19:26:58 +0530

    adding two new photos

--- src/app/components/photos/photos-page/photos-page.component.ts
+++ src/app/components/photos/photos-page/photos-page.component.ts
@@ -21,6 +21,24 @@ export class PhotosComponent implements OnInit {
       //   "PhotoAlbums": "Minimal"
       // },
       {
+        "PhotoName": "Details worth capturing",
+        "PhotoDescription": "Got this new webcam called Opal Camera this week. The details & auto-brightness/contrast/saturation on this thing is just mind-blowing. But it does get hot while using. If you're a tech nerd and want to upgrade your webcam situation, do check out this camera. It's pricy, mind you.",
+        "PhotoImage": "../assets/photos/2022-03-22-2.jpeg",
+        "PhotoLocation": "Bengaluru, India",
+        "PhotoDateTaken": "March 22, 2022",
+        "PhotoDateUploaded": "March 22, 2022",
+        "PhotoAlbums": "Minimal"
+      },
+      {
+        "PhotoName": "Watching the night",
+        "PhotoDescription": "Closeup shot of my Apple Watch Series 6",
+        "PhotoImage": "../assets/photos/2020-11-22-1.jpeg",
+        "PhotoLocation": "Bengaluru, India",
+        "PhotoDateTaken": "November 22, 2020",
+        "PhotoDateUploaded": "March 22, 2022",
+        "PhotoAlbums": "Minimal"
+      },
+      {
         "PhotoName": "Mini-nature & massive-tech",
         "PhotoDescription": "Random potrait shot of my iPad Pro. Got this iPad in 2019 from Singapore and have loved the iPad experience since then. Will always recommend anyone thinking of buying an iPad to write or sketch.",
         "PhotoImage": "../assets/photos/2020-11-13-2.jpeg",
