bf14117576a66c87bae5bc63583d1226663ad0e1
Sankalp Sinha <sankalpsinha@hey.com>
2022-04-30 18:46:27 +0530

    adding new footer with page links, current projects, & social links

--- src/app/components/footer-dark/footer-dark.component.ts
+++ src/app/components/footer-dark/footer-dark.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'component-footer-dark',
   templateUrl: './footer-dark.component.html',
-  styleUrls: ['./footer-dark.component.css']
 })
 export class FooterDarkComponent implements OnInit {
 
