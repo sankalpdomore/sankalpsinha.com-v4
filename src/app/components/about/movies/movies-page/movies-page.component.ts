fad1538e91fe28a4445e6ae99a7aad6530bfcef2
Sankalp Sinha <sankalpsinha@hey.com>
2022-05-07 15:05:34 +0530

    added 6 movies in movies page and related css

--- src/app/components/about/movies/movies-page/movies-page.component.ts
+++ src/app/components/about/movies/movies-page/movies-page.component.ts
@@ -13,39 +13,54 @@ export class MoviesPageComponent implements OnInit {
 
     [
       {
-        "PhotoName": "Details worth capturing",
-        "PhotoDescription": "Got this new webcam called Opal Camera this week. The details & auto-brightness/contrast/saturation on this thing is just mind-blowing. But it does get hot while using. If you're a tech nerd and want to upgrade your webcam situation, do check out this camera. It's pricy, mind you.",
-        "PhotoImage": "../assets/photos/2022-03-22-2.jpeg",
-        "PhotoLocation": "Bengaluru, India",
-        "PhotoDateTaken": "March 22, 2022",
-        "PhotoDateUploaded": "March 22, 2022",
-        "PhotoAlbums": "Minimal"
+        "MoviesName": "Doctor Strange in the Multiverse of Madness",
+        "MoviesImage": "../assets/about/movies/doctorstrangeinthemultiverseofmadness.jpg",
+        "MoviesDescription": "Sam Raimi, the director of the movie overdid the horror part. The movie didn't feel like the typical light-hearted Marvel movie that I go to theaters for.",
+        "MoviesWatchedDate": "May 6, 2022",
+        "MoviesReleasedDate": "May 6, 2022",
       },
       {
-        "PhotoName": "Watching the night",
-        "PhotoDescription": "Closeup shot of my Apple Watch Series 6",
-        "PhotoImage": "../assets/photos/2020-11-22-1.jpeg",
-        "PhotoLocation": "Bengaluru, India",
-        "PhotoDateTaken": "November 22, 2020",
-        "PhotoDateUploaded": "March 22, 2022",
-        "PhotoAlbums": "Minimal"
+        "MoviesName": "Constantine",
+        "MoviesImage": "../assets/about/movies/constantine.jpg",
+        "MoviesDescription": "Okish direction. Watched it only for Keanu Reeves.",
+        "MoviesWatchedDate": "May 3, 2022",
+        "MoviesReleasedDate": "February 25, 2005",
       },
       {
-        "PhotoName": "Mini-nature & massive-tech",
-        "PhotoDescription": "Random potrait shot of my iPad Pro. Got this iPad in 2019 from Singapore and have loved the iPad experience since then. Will always recommend anyone thinking of buying an iPad to write or sketch.",
-        "PhotoImage": "../assets/photos/2020-11-13-2.jpeg",
-        "PhotoLocation": "Bengaluru, India",
-        "PhotoDateTaken": "November 13, 2020",
-        "PhotoDateUploaded": "March 6, 2022",
-        "PhotoAlbums": "Minimal"
+        "MoviesName": "WeCrashed",
+        "MoviesImage": "../assets/about/movies/wecrashed.jpg",
+        "MoviesDescription": "The story telling & acting by Jared Leto & Anne Hathaway is on point. A MUST WATCH if you're interested in either well documented stories & startups.",
+        "MoviesWatchedDate": "April 28, 2022",
+        "MoviesReleasedDate": "March 18, 2022",
       },
+      {
+        "MoviesName": "SpiderMan: No way home",
+        "MoviesImage": "../assets/about/movies/spidermannowayhome.jpg",
+        "MoviesDescription": "Marvel's best multiverse movie so far as it was able to pull off the unimaginable task of brining all previous Spiderman & their counterparts in one movie. Very well executed storyline.",
+        "MoviesWatchedDate": "December 18, 2021",
+        "MoviesReleasedDate": "December 16, 2021",
+      },
+      {
+        "MoviesName": "Finch",
+        "MoviesImage": "../assets/about/movies/finch.jpg",
+        "MoviesDescription": "Upbeat and hopeful storytelling",
+        "MoviesWatchedDate": "December 13, 2021",
+        "MoviesReleasedDate": "November 5, 2021",
+      },
+      {
+        "MoviesName": "13 Hours",
+        "MoviesImage": "../assets/about/movies/13hours.jpg",
+        "MoviesDescription": "Loved the fact that it didn't have an all-star cast back then and still all the actors felt perfect for their roles. The action sequences were directed to feel real 100%.",
+        "MoviesWatchedDate": "May 24, 2021",
+        "MoviesReleasedDate": "January 15, 2016",
+      }
     ]
 
   constructor(
     private titleService: Title,
     private meta: Meta
   ) {
-    this.titleService.setTitle('Sankalp Sinha • Photos');
+    this.titleService.setTitle('Sankalp Sinha • Movies');
     this.meta.addTag({ name: 'description', content: 'Since the day I got a camera phone, I was hooked on to clicking pictures. With better camera phones and photo sharing apps like Instagram, I shared more pictures. But, lately, Instagram does not feel like a place to share photos anymore. It feels more like an app to create dance and entertainment videos and reach more dopamine addicted users. This page is my reaction to the disappointment Instagram has become. I love clicking pictures and sharing them, and finally, I have a place for them here. 🙌' });
     this.meta.addTag({ name: 'keywords', content: '' });
   }
