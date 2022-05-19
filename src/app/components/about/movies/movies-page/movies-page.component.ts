bbcc14c8704bec9405e93880791ed248ba7a8de7
Sankalp Sinha <sankalpsinha@hey.com>
2022-05-19 17:32:15 +0530

    Renamed movies title meta Added selection css to be orange overall on the site

--- src/app/components/about/movies/movies-page/movies-page.component.ts
+++ src/app/components/about/movies/movies-page/movies-page.component.ts
@@ -17,7 +17,7 @@ export class MoviesPageComponent implements OnInit {
     private titleService: Title,
     private meta: Meta
   ) {
-    this.titleService.setTitle('Sankalp Sinha • Movies');
+    this.titleService.setTitle('Sankalp Sinha • Movies & Shows');
     this.meta.addTag({ name: 'description', content: 'Movies, TV shows, and documentories, have always been a doorway to creative thinking for me. Seeing how directors & artists around the world can percieve something so differently and creatively has always been inspiring for me as a creator. That is why I love watching movies, TV shows, or any other kind of media. This page is an attempt to capture everything I have watched so far.' });
     this.meta.addTag({ name: 'keywords', content: '' });
   }
